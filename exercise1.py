64b0c03f5a07397bbff7bb170ab0311fe19faea0
Tanya Thakur <71583466+tanya1019@users.noreply.github.com>
2025-10-28 16:58:59 +0100

    fixed test_3d_vector_subtraction (#2)

--- exercise1.py
+++ exercise1.py
@@ -45,5 +45,11 @@ def test_3d_vector_addition() -> None:
 
 
 def test_3d_vector_subtraction() -> None:
-    # Task B: add a test for vector subtraction
-    assert False
+    v1 = Vector([5.0, 7.0, 9.0])
+    v2 = Vector([1.0, 2.0, 3.0])
+
+    result = v1 - v2
+
+    assert isclose(result[0], 4.0)
+    assert isclose(result[1], 5.0)
+    assert isclose(result[2], 6.0)
